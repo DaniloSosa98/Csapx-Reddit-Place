a6167273aaba76cc9074aa9e9f999e0b87c3c6b2
ayt4797 <ayt4797@rit.edu>
2019-11-21 12:35:20 -0500

    Connected the servers :D

--- src/place/client/ptui/PlacePTUI.java
+++ src/place/client/ptui/PlacePTUI.java
@@ -4,15 +4,45 @@ import place.PlaceTile;
 import place.model.ClientModel;
 import place.model.Observer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class PlacePTUI implements Observer<ClientModel, PlaceTile> {
+    /**
+     * Connection to network interface to server
+     */
 
     @Override
     public void update(ClientModel model, PlaceTile tile) {
+
     }
 
     public static void main(String[] args) {
         if (args.length != 3) {
             System.out.println("Usage: java PlaceClient host port username");
+        }
+        // Get host info from command line
+        String host = args[1];
+        int port = Integer.parseInt(host);
+        try (
+                Socket plySocket1 = new Socket(args[0], port); //plyr 1
+//                Socket plySocket2 = new Socket(hostName, portNumber); //plyr 1
+
+                PrintWriter out = new PrintWriter(plySocket1.getOutputStream(), true);
+//                PrintWriter out2 =new PrintWriter(plySocket2.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(plySocket1.getInputStream())); //fr server
+//                BufferedReader in2 =new BufferedReader(new InputStreamReader(plySocket2.getInputStream())); //fr server
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in)); //for the local comp
+
+        ) {while(true) {
+            out.println("is connected");
+            String details = in.readLine();
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/place/server/PlaceServer.java
+++ src/place/server/PlaceServer.java
@@ -1,5 +1,12 @@
 package place.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * The Place server is run on the command line as:
  *
@@ -21,6 +28,22 @@ public class PlaceServer {
         if (args.length != 2) {
             System.out.println("Usage: java PlaceServer port DIM");
         } else {
+            while(true) {
+                try (
+                        ServerSocket ss1 = new ServerSocket(Integer.parseInt(args[1]));
+                        Socket socket = ss1.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                ) {
+                    if (socket != null) {
+                        System.out.println("player 1 connected");
+                        System.out.println(in.readLine());
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
